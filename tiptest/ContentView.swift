cfa5396d11a6afb09ff823840725507c4f38f8ea
Nikolai Trukhin <NTrukhin@alfabank.ru>
2023-08-04 19:55:26 +0200

    Initial Commit

---
+++ tiptest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tiptest
+//
+//  Created by Nikolai Trukhin on 04.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tiptest/tiptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiptestApp.swift
+//  tiptest
+//
+//  Created by Nikolai Trukhin on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct tiptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
