cdd84ab6a2238fff2488803ab5e1ac1b4e4c6243
Nikolai Trukhin <coolone.official@gmail.com>
2023-08-08 14:03:53 +0200

    Add screenshot

---
+++ README.md
@@ -0,0 +1,6 @@
+# TipKit sample
+
+Example of using TipKit with SwiftUI
+
+<img src="screenshot.png" width=250>
+
--- tiptest/tiptestApp.swift
+++ tiptest/tiptestApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import TipKit
 
 @main
 struct tiptestApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+
+    init() {
+        Task {
+            try? await Tips.configure {
+                /// The frequency your tips display.
+                /// .immediate, .hourly, .daily, .weekly, .monthly or custom TimeInterval
+                DisplayFrequency(.immediate)
+                
+                /// A location for persisting your application's tips and associated data.
+                DatastoreLocation(.applicationDefault)
+            }
+            
+            Tips.showAllTips()
         }
     }
 }
